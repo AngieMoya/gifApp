e16a7488195d6b11097bf8e2ced907de62b8e607
Angie Moya <angiemoyau.1998@gmail.com>
2025-08-30 09:55:10 -0500

    gitApp initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  //ApiKeys
+  giphyApiKey: 'vSstgs9bJgtVHUnvnBl7IOO6jhkb6k56',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
